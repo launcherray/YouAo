bb11679ae5db3450feb76bbd4dd9bb93bc1c83d3
launcherray <107042253+launcherray@users.noreply.github.com>
2022-08-07 21:58:23 +0800

    Add files via upload

---
+++ work2/work4.java
@@ -0,0 +1,12 @@
+public class work4 { 
+
+	public static void main(String[] args) {
+		String name = "YouAo";
+		int age = 21;
+		double score = 90;
+		char gender = 'm';
+		String hobby = "sleepping";
+		System.out.println("姓名\t年龄\t成绩\t性别\t爱好\n" + name + "\t" 
+			+ age + "\t" + score + "\t" + gender + "\t" + hobby);
+	}
+}
